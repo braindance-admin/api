b5e0542b5009c7152d7815c870530d06979f95a8
wires <jelle@defekt.nl>
2016-03-05 14:10:10 +0100

    added some resources

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var app = express()
 
 app.use('/', express.static('../frontend'))
 
-var data = []
+var tracks = []
+var grouped = {}
 
 glob.glob('../scadm2/pending/*.meta.json', function (err, files) {
   if (err) {
@@ -16,17 +17,57 @@ glob.glob('../scadm2/pending/*.meta.json', function (err, files) {
   }
 
   // parse contents, make into large array
-  data = R.map(function (filename) {
-    console.log(filename)
+  tracks = R.map(function (filename) {
+    console.log('processing: ', filename)
     var contents = fs.readFileSync(filename)
     var metadata = JSON.parse(contents)
     return metadata
   }, files)
+
+  grouped = R.pipe(
+    // group by user id
+    R.groupBy(R.path(['user', 'permalink'])),
+    // create a 'toplevel' user object, by picking it from the tracks
+    R.map(function (user_tracks) {
+      return {
+        tracks: user_tracks,
+        user: R.head(user_tracks).user
+      }
+    })
+  )(tracks)
+
+  console.log('try localhost:3000/')
 })
 
 // serve the array
-app.get('/data.json', function (req, res) {
-  res.json(data)
+app.get('/all-tracks', function (req, res) {
+  res.json(tracks)
+})
+
+app.get('/users', function (req, res) {
+  var users = R.keys(grouped)
+  var user_with_count = R.map(function (username) {
+    return {
+      user_id: username,
+      track_count: grouped[username].tracks.length
+    }
+  }, users)
+
+  res.json(user_with_count)
+})
+
+app.get('/tracks-from-user/:user_id', function (req, res) {
+  var user_id = req.params.user_id
+
+  if (R.has(user_id, grouped)) {
+    res.json(grouped[user_id])
+  } else {
+    res.json([])
+  }
+})
+
+app.get('/grouped-by-user', function (req, res) {
+  res.json(grouped)
 })
 
 app.listen(3000)
